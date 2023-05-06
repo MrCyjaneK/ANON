91a827d2ed2958d62f851d19bd04c0d908073694
anonym <0xanonym@proton.me>
2023-05-06 23:05:26 +0530

    added config

---
+++ lib/models/config.dart
@@ -0,0 +1,13 @@
+import 'package:json_annotation/json_annotation.dart';
+
+@JsonSerializable()
+class _AnonConfigState {
+  bool _isViewOnly = false;
+
+  get isViewOnly => _isViewOnly;
+
+  setWalletView(bool isViewOnly) => _isViewOnly = isViewOnly;
+}
+
+
+final anonConfigState = _AnonConfigState();
